4fb51a6b0c1e5fa20e43e4c725a4a801aa96713b
Shihao Hu <2462833786@qq.com>
2024-08-22 20:23:50 +0800

    完成用户服务拆分

---
+++ user-service/src/main/java/com/hmall/user/UserApplication.java
@@ -0,0 +1,17 @@
+package com.hmall.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shihaohu
+ * @date 2024/8/22 19:53
+ */
+@MapperScan("com.hmall.user.mapper")
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
