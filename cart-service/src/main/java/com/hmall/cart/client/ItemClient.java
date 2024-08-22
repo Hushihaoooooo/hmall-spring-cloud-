2da5425ecb26a14781c38c25e04f35a85fb5167e
Shihao Hu <2462833786@qq.com>
2024-08-22 15:25:30 +0800

    openfeign快速远程调用

---
+++ cart-service/src/main/java/com/hmall/cart/client/ItemClient.java
@@ -0,0 +1,19 @@
+package com.hmall.cart.client;
+
+import com.hmall.cart.domain.dto.ItemDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Shihaohu
+ * @date 2024/8/22 14:35
+ */
+@FeignClient("item-service")
+public interface ItemClient {
+    @GetMapping("/items")
+    List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
+}
--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.cart.client.ItemClient;
 import com.hmall.cart.domain.dto.CartFormDTO;
 import com.hmall.cart.domain.dto.ItemDTO;
 import com.hmall.cart.domain.po.Cart;
@@ -43,9 +44,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
-    private final RestTemplate restTemplate;
-
-    private final DiscoveryClient discoveryClient;
+    private final ItemClient itemClient;
 
     //private final IItemService itemService;
 
@@ -91,28 +90,7 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         // 1.获取商品id TODO 处理商品信息
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
-        //List<ItemDTO> items = itemService.queryItemByIds(itemIds);
-        // 2.1 发现item-service服务的实例列表
-        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
-        if (CollUtils.isEmpty(instances)) {
-            return ;
-        }
-        // 2.2 负载均衡，挑选一个实例
-        ServiceInstance serviceInstance = instances.get(RandomUtil.randomInt(instances.size()));
-
-        // 2.3 发送请求，查询商品
-        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                serviceInstance.getUri() + "/items?ids={ids}",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<ItemDTO>>() {
-                },
-                Map.of("ids", CollUtil.join(itemIds, ","))
-        );
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            return ;
-        }
-        List<ItemDTO> items = response.getBody();
+        List<ItemDTO> items = itemClient.queryItemByIds(itemIds);
         if (CollUtils.isEmpty(items)) {
             throw new BadRequestException("购物车中商品不存在！");
         }
