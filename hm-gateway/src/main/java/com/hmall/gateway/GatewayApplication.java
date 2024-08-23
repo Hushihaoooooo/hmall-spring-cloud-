6fc1cda33dbe6d42f20facd3c1e67c53733a841f
Shihao Hu <2462833786@qq.com>
2024-08-23 15:02:14 +0800

    完成服务拆分，配置网关。

---
+++ hm-gateway/src/main/java/com/hmall/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shihaohu
+ * @date 2024/8/23 14:40
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
