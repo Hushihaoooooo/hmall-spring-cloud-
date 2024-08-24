bb96efb9dabeb0f8f45544f494e318fdaa935b38
Shihao Hu <2462833786@qq.com>
2024-08-24 19:26:57 +0800

    自定义过滤器GlobalFilter

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/MyGlobalFilter.java
@@ -0,0 +1,29 @@
+package com.hmall.gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Shihaohu
+ * @date 2024/8/24 18:47
+ */
+@Component
+public class MyGlobalFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        System.out.println("执行了自定义的全局过滤器");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
