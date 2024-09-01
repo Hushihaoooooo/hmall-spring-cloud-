30f15e509e17354c434849afb1c61a45d6d17b27
Shihao Hu <2462833786@qq.com>
2024-09-01 14:39:51 +0800

    解决超时订单问题

---
+++ trade-service/src/main/java/com/hmall/trade/constants/MqConstants.java
@@ -0,0 +1,11 @@
+package com.hmall.trade.constants;
+
+/**
+ * @author Shihaohu
+ * @date 2024/9/1 14:11
+ */
+public interface MqConstants {
+    String DELAY_EXCHANGE_NAME = "trade.delay.direct";
+    String DELAY_ORDER_QUEUE_NAME = "trade.delay.order.queue";
+    String DELAY_ORDER_KEY = "delay.order.query";
+}
--- trade-service/src/main/java/com/hmall/trade/service/impl/OrderServiceImpl.java
+++ trade-service/src/main/java/com/hmall/trade/service/impl/OrderServiceImpl.java
@@ -7,6 +7,7 @@ import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
 import com.hmall.common.exception.BadRequestException;
 import com.hmall.common.utils.UserContext;
+import com.hmall.trade.constants.MqConstants;
 import com.hmall.trade.domain.dto.OrderFormDTO;
 import com.hmall.trade.domain.po.Order;
 import com.hmall.trade.domain.po.OrderDetail;
@@ -15,6 +16,7 @@ import com.hmall.trade.service.IOrderDetailService;
 import com.hmall.trade.service.IOrderService;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,6 +42,7 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     private final IOrderDetailService detailService;
     private final ItemClient itemClient;
     private final CartClient cartClient;
+    private final RabbitTemplate rabbitTemplate;
 
 
     @Override
@@ -84,6 +87,19 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
         } catch (Exception e) {
             throw new RuntimeException("库存不足！");
         }
+
+        // TODO 5.发送延迟消息，用于检查订单是否支付成功
+        rabbitTemplate.convertAndSend(
+                MqConstants.DELAY_EXCHANGE_NAME,
+                MqConstants.DELAY_ORDER_KEY,
+                order.getId(),
+                message -> {
+                    message.getMessageProperties().setDelay(10000);
+                    return message;
+                }
+        );
+
+
         return order.getId();
     }
 
@@ -94,6 +110,11 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
         order.setStatus(2);
         order.setPayTime(LocalDateTime.now());
         updateById(order);
+    }
+
+    @Override
+    public void cancelOrder(Long orderId) {
+
     }
 
     private List<OrderDetail> buildDetails(Long orderId, List<ItemDTO> items, Map<Long, Integer> numMap) {
